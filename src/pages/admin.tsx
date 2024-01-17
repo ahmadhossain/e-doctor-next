01e78f43c3c1951b7531fa0a19e8891fddfce317
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2024-01-17 18:55:49 +0600

    doctor verify

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Space, Table, Tag } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Space, Table, Tag } from "antd";
 import type { ColumnsType } from "antd/es/table";
 
 interface DataType {
@@ -13,92 +13,126 @@ interface DataType {
   type: string;
 }
 
-const columns: ColumnsType<DataType> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Mobile",
-    dataIndex: "mobile",
-    key: "mobile",
-  },
-  {
-    title: "Gender",
-    dataIndex: "gender",
-    key: "gender",
-  },
-  {
-    title: "Nid",
-    dataIndex: "nid",
-    key: "nid",
-  },
-  {
-    title: "Registration Number (BMDC)",
-    dataIndex: "bmdc",
-    key: "bmdc",
-  },
-  {
-    title: "Doctor Type",
-    dataIndex: "type",
-    key: "type",
-  },
+// const data: DataType[] = [
+//   {
+//     key: "1",
+//     name: "Buse Solmaz",
+//     email: "buse.solmaz@example.com",
+//     mobile: "01646767921",
+//     gender: "female",
+//     nid: "658982346975345",
+//     bmdc: "82346975345",
+//     type: "Medical",
+//   },
+//   {
+//     key: "2",
+//     name: "Philippe Grewal",
+//     email: "philippe.grewal@example.com",
+//     mobile: "01546364972",
+//     gender: "male",
+//     nid: "658982346975345",
+//     bmdc: "82346975345",
+//     type: "Dental",
+//   },
+//   {
+//     key: "3",
+//     name: "Ines Norberg",
+//     email: "ines.norberg@example.com",
+//     mobile: "01346517939",
+//     gender: "female",
+//     nid: "658982346975345",
+//     bmdc: "82346975345",
+//     type: "Veterinary",
+//   },
+// ];
 
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <a>approve</a>
-      </Space>
-    ),
-  },
-];
+const AdminPage = () => {
+  const [data, setData] = useState([]);
 
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "Buse Solmaz",
-    email: "buse.solmaz@example.com",
-    mobile: "01646767921",
-    gender: "female",
-    nid: "658982346975345",
-    bmdc: "82346975345",
-    type: "Medical",
-  },
-  {
-    key: "2",
-    name: "Philippe Grewal",
-    email: "philippe.grewal@example.com",
-    mobile: "01546364972",
-    gender: "male",
-    nid: "658982346975345",
-    bmdc: "82346975345",
-    type: "Dental",
-  },
-  {
-    key: "3",
-    name: "Ines Norberg",
-    email: "ines.norberg@example.com",
-    mobile: "01346517939",
-    gender: "female",
-    nid: "658982346975345",
-    bmdc: "82346975345",
-    type: "Veterinary",
-  },
-];
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "fullName",
+      key: "fullName",
+      render: (text) => <div className="text-blue-500">{text}</div>,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Mobile",
+      dataIndex: "mobile",
+      key: "mobile",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "Nid",
+      dataIndex: "nid",
+      key: "nid",
+    },
+    {
+      title: "Registration Number (BMDC)",
+      dataIndex: "regNumBMDC",
+      key: "regNumBMDC",
+    },
+    {
+      title: "Doctor Type",
+      dataIndex: "doctorType",
+      key: "doctorType",
+    },
 
-const AdminPage: React.FC = () => (
-  <div className="p-10">
-    <Table columns={columns} dataSource={data} />
-  </div>
-);
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button
+            onClick={() => handleSubmit(record.email)}
+            className="text-green-500"
+          >
+            approve
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  const handleSubmit = async (email: string) => {
+    const res = await fetch(
+      `http://localhost:8080/api/doctors/verify/${email}`,
+      {
+        cache: "no-store",
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    setData(data);
+  };
+
+  useEffect(() => {
+    const getUnverifiedDoctor = async () => {
+      const res = await fetch(`http://localhost:8080/api/doctors/unverified`, {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      console.log(data);
+      setData(data);
+    };
+
+    getUnverifiedDoctor();
+  }, []);
+
+  return (
+    <div className="p-10">
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
+};
 
 export default AdminPage;
